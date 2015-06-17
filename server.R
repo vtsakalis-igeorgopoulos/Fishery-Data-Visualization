1bbc6c50c2ba20c87ecb0bd1584138fe900f538b
vtsakalis-igeorgopoulos <vtsakalis-igeorgopoulos@users.noreply.github.com>
2015-06-17 14:00:52 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,18 +1,19 @@
-> library(googleVis)
 
-> library(shiny)
+library(googleVis)
+
+library(shiny)
 
 #import data
-> Fishery_data=read.csv2("mydata2.csv", header = FALSE)
+Fishery_data=read.csv2("mydata2.csv", header = FALSE)
 
-> col_names<-c("Quantity","Year","Region")
+col_names<-c("Quantity","Year","Region")
 
 #rename columns
-> colnames(Fishery_data)<-col_names
+colnames(Fishery_data)<-col_names
 
 #create motion
-> Motion=gvisMotionChart(Fishery_data,idvar="Region",timevar="Year",options=list(state='{"colorOption":"_UNIQUE_COLOR","showTrails":false};'))
-> plot(Motion)
+Motion=gvisMotionChart(Fishery_data,idvar="Region",timevar="Year",options=list(state='{"colorOption":"_UNIQUE_COLOR","showTrails":false};'))
+plot(Motion)
 
 shinyServer(
     function(input, output) {
